d8f596dfd17e9ad602d9d1d607066dd9f619fead
juanis2112 <juanitagomezr2112@gmail.com>
2020-08-17 01:01:27 -0500

    Add content to tour for overview in docs

--- doc/_static/custom_scripts.js
+++ doc/_static/custom_scripts.js
@@ -36,29 +36,118 @@
     function setupTourDriver() {
         driver.defineSteps([
           {
-            element: "#editor-rect",
+            element: "#introduction-rect",
             popover: {
-              title: "Editor",
-              description: "Spyder's Editor is really really slow",
+              title: "Spyder",
+              description: 'Spyder is a powerful scientific IDE for Python. We will guide you through some of its most important features.',
               position: "right",
             },
           },
           {
-            element: "#console-rect",
+            element: "#toolbar-rect",
             popover: {
-              title: "IPython Console",
-              description: "The IPython Console runs code",
-              position: "left",
+              title: "Toolbar",
+              description: 'The toolbar allows you to quickly access some of the most common commands in Spyder.',
+              position: "bottom",
+            },
+          },
+          {
+            element: "#statusbar-rect",
+            popover: {
+              title: "Status Bar",
+              description: 'The status bar shows your current Python environment, git branch, memory usage and various attributes of your current file.',
+              position: "top",
             },
           },
           {
             element: "#options-menu-rect",
             popover: {
               title: "Options Menu",
-              description: "Each pane has an options menu",
+              description: 'You can display the options menu by clicking the hamburger icon at the top right of the pane. It contains useful options and actions relevant to the pane.',
+              position: "right",
+            },
+          },
+          {
+            element: "#context-menu-rect",
+            popover: {
+              title: "Context Menu",
+              description: 'To display the context menu for a pane, right-click anywhere over it. The menu shows actions relevant to the element you are hovering over.',
+              position: "right",
+            },
+          },
+          {
+            element: "#editor-rect",
+            popover: {
+              title: "Editor",
+              description: 'The <a href="editor.html">Editor</a> is the pane where you can create, open and edit files. It contains useful features like autocompletion, real-time analysis and syntax highlighting.',
+              position: "right",
+            },
+          },
+          {
+            element: "#console-rect",
+            popover: {
+              title: "Ipython Console",
+              description: 'The <a href="ipythonconsole.html">Console</a> allows you to run your code from the Editor or interactively. You can also use it to interact with Spyder’s debugger.',
               position: "left",
             },
           },
+          {
+            element: "#help-rect",
+            popover: {
+              title: "Help",
+              description: 'The <a href="help.html">Help</a> pane displays documentation for the objects you are using in the Editor or the IPython Console. To use it, press Ctrl-I (Cmd-I on macOS) with your cursor over an object, or type its name in the object field.',
+              position: "left",
+            },
+          },
+          {
+            element: "#variable-explorer-rect",
+            popover: {
+              title: "Variable Explorer",
+              description: 'The <a href="variableexplorer.html">Variable Explorer</a> allows you to browse and interact with the objects generated running your code. Double-clicking a variable will open a specialized viewer, allowing you to inspect its contents.',
+              position: "left",
+            },
+          },
+          {
+            element: "#plots-rect",
+            popover: {
+              title: "Plots",
+              description: 'The <a href="plots.html">Plots</a> pane shows the figures and images created during your code execution. It allows you to browse, zoom, copy, and save the generated plots.',
+              position: "left",
+            },
+          },
+          {
+            element: "#files-rect",
+            popover: {
+              title: "Files",
+              description: 'The <a href="fileexplorer.html">Files</a> pane lets you browse the contents of the directories on your computer, open them in the Editor, and perform a variety of other file operations.',
+              position: "left",
+            },
+          },
+          {
+            element: "#find-rect",
+            popover: {
+              title: "Find",
+              description: 'The <a href="findinfiles.html">Find</a> pane allows you to view and navigate through all occurrences of your search text in a directory.',
+              position: "left",
+            },
+          },
+          {
+            element: "#profiler-rect",
+            popover: {
+              title: "Profiler",
+              description: 'The <a href="profiler.html">Profiler</a> helps you optimize your code by determining the run time and number of calls for every function and method used in a file. It allows you to save and compare your results between runs.',
+              position: "left",
+            },
+          },
+          {
+            element: "#code-analysis-rect",
+            popover: {
+              title: "Code Analysis",
+              description: 'The <a href="pylint.html">Code Analysis</a> helps you improve the quality of your programs by detecting style issues, bad practices and potential bugs.',
+              position: "left",
+            },
+          },
+
         ]);
 
         document.getElementById("quickstart-tour-start").onclick = startTour;
--- doc/quickstart.rst
+++ doc/quickstart.rst
@@ -7,11 +7,29 @@ Quickstart
    <div class="interactive-tour-container">
       <img id="tour-default-layout-main" class="tour-screenshot tour-screenshot-active" src="_static/tour/default-layout-main.png">
       <img id="tour-options-menu" class="tour-screenshot" src="_static/tour/options-menu.png">
-
+      <img id="tour-context-menu" class="tour-screenshot" src="_static/tour/context-menu.png">
+      <img id="tour-variable-explorer" class="tour-screenshot" src="_static/tour/variable-explorer.png">
+      <img id="tour-plots" class="tour-screenshot" src="_static/tour/plots.png">
+      <img id="tour-files" class="tour-screenshot" src="_static/tour/files.png">
+      <img id="tour-find" class="tour-screenshot" src="_static/tour/find.png">
+      <img id="tour-profiler" class="tour-screenshot" src="_static/tour/profiler.png">
+      <img id="tour-code-analysis" class="tour-screenshot" src="_static/tour/code-analysis.png">
+      
       <svg class="tour-overlay" viewBox="0 0 1280 720">
+        <rect id="introduction-rect" class="highlight-rect tour-default-layout-main" x="5" y="5" width="1270" height="710" />
+        <rect id="toolbar-rect" class="highlight-rect tour-default-layout-main" x="5" y="5" width="1270" height="32"" />
+        <rect id="statusbar-rect" class="highlight-rect tour-default-layout-main" x="5" y="690" width="1270" height="25" /> 
+        <rect id="options-menu-rect" class="highlight-rect tour-options-menu" x="610" y="60" width="250" height="410" />
+        <rect id="context-menu-rect" class="highlight-rect tour-context-menu" x="210" y="240" width="280" height="410" />
         <rect id="editor-rect" class="highlight-rect tour-default-layout-main" x="5" y="35" width="635" height="660" />
         <rect id="console-rect" class="highlight-rect tour-default-layout-main" x="635" y="335" width="640" height="360" />
-        <rect id="options-menu-rect" class="highlight-rect tour-options-menu" x="980" y="335" width="295" height="335" />
+        <rect id="help-rect" class="highlight-rect tour-default-layout-main" x="635" y="35" width="640" height="305" />
+        <rect id="variable-explorer-rect" class="highlight-rect tour-variable-explorer" x="635" y="35" width="640" height="305" />
+        <rect id="plots-rect" class="highlight-rect tour-plots" x="635" y="35" width="640" height="305" />
+        <rect id="files-rect" class="highlight-rect tour-files" x="635" y="35" width="640" height="305" />
+        <rect id="find-rect" class="highlight-rect tour-find" x="635" y="35" width="640" height="305" />
+        <rect id="profiler-rect" class="highlight-rect tour-profiler" x="635" y="35" width="640" height="305" />
+        <rect id="code-analysis-rect" class="highlight-rect tour-code-analysis" x="635" y="35" width="640" height="305" />
       </svg>
    </div>
    <input id="quickstart-tour-start" type="button" value="Restart Tour" />
