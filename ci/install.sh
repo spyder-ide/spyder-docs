0489b56b4278df5f11cf779c7e0498cbe21ab399
dalthviz <d.althviz10@uniandes.edu.co>
2020-08-25 17:18:17 -0500

    Checkout all the repo branches using Travis

--- ci/install.sh
+++ ci/install.sh
@@ -2,4 +2,4 @@
 
 pip3 install -U pip
 pip3 install sphinx doctr sphinx-panels sphinx-multiversion
-pip3 install git+https://github.com/spyder-ide/spyder-docs-sphinx-theme.git@develop_spyder
+pip3 install git+https://github.com/dalthviz/spyder-docs-sphinx-theme.git@sidebar_widgets
