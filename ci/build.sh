c931c3a7350a7ff1cb1bee289e34b4b9117d579a
dalthviz <d.althviz10@uniandes.edu.co>
2020-08-25 23:07:52 -0500

    Use pip commands directly

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 
-.install.sh
+pip3 install -U pip
+pip3 install sphinx doctr sphinx-panels
+pip3 install git+https://github.com/spyder-ide/spyder-docs-sphinx-theme.git@develop_spyder
 make docs
