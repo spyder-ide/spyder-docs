1fc9a405cb47b5a63107800bf9b818ff5c4cf850
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2020-08-26 15:48:52 -0500

    Refactor CI config and scripts to avoid duplicated code

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -ex
 
-pip3 install -U pip
-pip3 install sphinx doctr sphinx-panels sphinx-multiversion
-# You can change the command above to use a different version of the theme (but return it to the correct one before merging)
-pip3 install git+https://github.com/dalthviz/spyder-docs-sphinx-theme.git@sidebar_widgets
 make multidocs
--- ci/install.sh
+++ ci/install.sh
@@ -2,4 +2,5 @@
 
 pip3 install -U pip
 pip3 install sphinx doctr sphinx-panels sphinx-multiversion
-pip3 install git+https://github.com/spyder-ide/spyder-docs-sphinx-theme.git@develop_spyder
+# You can change the command above to use a different version of the theme (but return it to the correct one before merging)
+pip3 install git+https://github.com/dalthviz/spyder-docs-sphinx-theme.git@sidebar_widgets
