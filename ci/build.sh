ec9773eb3273881731bc38962487cb46d38029a5
dalthviz <d.althviz10@uniandes.edu.co>
2020-08-25 23:27:34 -0500

    Update install and build scripts

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 pip3 install -U pip
-pip3 install sphinx doctr sphinx-panels
-pip3 install git+https://github.com/spyder-ide/spyder-docs-sphinx-theme.git@develop_spyder
-make docs
+pip3 install sphinx doctr sphinx-panels sphinx-multiversion
+# You can change the command above to use a different version of the theme (but return it to the correct one before merging)
+pip3 install git+https://github.com/dalthviz/spyder-docs-sphinx-theme.git@sidebar_widgets
+make multidocs
