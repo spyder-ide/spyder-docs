0f3a98950614c6c68b4da93ed08fcc0c0c7cc9b5
dalthviz <d.althviz10@uniandes.edu.co>
2020-08-26 23:36:21 -0500

    Try to build more branches

--- ci/install.sh
+++ ci/install.sh
@@ -4,3 +4,11 @@ pip3 install -U pip
 pip3 install sphinx doctr sphinx-panels sphinx-multiversion
 # You can change the command above to use a different version of the theme (but return it to the correct one before merging)
 pip3 install git+https://github.com/dalthviz/spyder-docs-sphinx-theme.git@sidebar_widgets
+if [ "${PULL_REQUEST-}" = "true" ]; then
+    git remote add origin https://github.com/spyder-ide/spyder-docs.git
+    git fetch origin
+    git branch pr
+    git checkout master
+    git checkout 3.x
+    git checkout pr
+fi
