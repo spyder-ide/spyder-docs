8bcd83586e7f32b4f1860c2e559d97b07c91d149
dalthviz <d.althviz10@uniandes.edu.co>
2020-08-29 22:29:23 -0500

    Reset git theme url to develop_spyder branch

--- ci/install.sh
+++ ci/install.sh
@@ -3,7 +3,7 @@
 pip3 install -U pip
 pip3 install sphinx doctr sphinx-panels sphinx-multiversion
 # You can change the command above to use a different version of the theme (but return it to the correct one before merging)
-pip3 install git+https://github.com/dalthviz/spyder-docs-sphinx-theme.git@sidebar_widgets
+pip3 install git+https://github.com/spyder-ide/spyder-docs-sphinx-theme.git@develop_spyder
 # Needed to build PR previews using Netlify with the different versions of the docs available
 if [ "${CI-}" = "true" ]; then
     git config --global user.email "you@example.com"
